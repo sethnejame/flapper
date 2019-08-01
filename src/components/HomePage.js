fdeacbd4b250ce98254c7f91715253faf1d405ec
Seth NeJame <sethnejame@gmail.com>
2019-08-01 15:31:06 +0200

    Adding first function component

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>The Most Amazing Website</h1>
+      <p>That was ever created in the history of mankind.</p>
+    </div>
+  );
+}
+
+export default HomePage;
