da9e7ffda2f89483e1de5a4b91228df4f1756d51
Seth NeJame <sethnejame@gmail.com>
2019-08-01 15:42:54 +0200

    Hard coding router

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h2>About</h2>
+        <p>This app uses react and runs on unicorn blood.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,6 +5,7 @@ function HomePage() {
     <div className="jumbotron">
       <h1>The Most Amazing Website</h1>
       <p>That was ever created in the history of mankind.</p>
+      <a href="/about">About Page</a>
     </div>
   );
 }
