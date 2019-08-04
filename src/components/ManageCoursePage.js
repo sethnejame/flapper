22e1a373fe0a20430186eddf1e56c060b2806ca0
Seth NeJame <sethnejame@gmail.com>
2019-08-04 15:45:51 +0200

    Adding form fields

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CourseForm course={course} />
     </>
   );
 };
