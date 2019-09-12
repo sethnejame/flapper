2a20b61267dee1516aede7eb0dde0e37034f6892
seth@mondido <sethnejame>
2019-09-12 14:16:47 +0200

    Adding toastify to handleSubmit function. . .awesome UI save confirmation

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
+import { toast } from 'react-toastify'
 
 const ManageCoursePage = props => {
   const [course, setCourse] = useState({
@@ -23,6 +24,7 @@ const ManageCoursePage = props => {
     e.preventDefault();
     courseApi.saveCourse(course).then(() => {
       props.history.push("/courses");
+      toast.success('Course saved.')
     });
   };
 
