106d5a32fa15a40a94e1cd092f82540f2e8c6f37
seth@mondido <sethnejame>
2019-09-15 17:51:32 +0200

    Registering delete coursewith dispatcher @ coureStor

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -31,6 +31,12 @@ const store = new CourseStore();
 
 Dispatcher.register(action => {
   switch (action.actionType) {
+    case actionTypes.DELETE_COURSE:
+      _courses = _courses.filter(
+        course => course.id !== parseInt(action.id, 10)
+      );
+      store.emitChange();
+      break;
     case actionTypes.CREATE_COURSE:
       _courses.push(action.course);
       store.emitChange();
@@ -40,7 +46,7 @@ Dispatcher.register(action => {
         course.id === action.course.id ? action.course : course
       );
       store.emitChange();
-      break; 
+      break;
     case actionTypes.LOAD_COURSES:
       _courses = action.courses;
       store.emitChange();
