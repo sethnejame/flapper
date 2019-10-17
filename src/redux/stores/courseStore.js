199a380bd941cac84ad6fd35e14134908866b6ca
seth@mondido <sethnejame>
2019-10-17 06:38:59 +0200

    Creating/configing  redux store for state mngmt

---
+++ src/redux/stores/courseStore.js
@@ -0,0 +1,20 @@
+// import createStore (duh), middleware (enhance redux behavior)
+// and compose, which is support for redux developer tools
+import { createStore, applyMiddleware, compose } from "redux";
+// notice we don't need to ref. 'index' for root reducer, it's auto
+import rootReducer from "./reducers";
+// we pass the above import to applyMiddleware - it warns us if we
+// try to mutate state in any way (state safety net)
+import reduxImmutableStateInvariant from "redux-immutable-state-inariant";
+
+export default function configStore(initState) {
+  // add support for redux dev tools
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  // config the store
+  return createStore(
+    rootReducer, // our reducer, which uses actions to change state
+    initState, // baseline state
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant())) // middleware
+  );
+}
